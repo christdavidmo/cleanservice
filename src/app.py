f92a1b33b3d14db648c38131eade22f3a2c00468
christdavidmo <christdavidmondjot6@gmail.com>
2025-03-11 15:29:39 +0000

    ajout modification getway

--- src/app.py
+++ src/app.py
@@ -1,8 +1,25 @@
+
+# from __init__ import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+  
+
+
+
+
+# import sys
+# import os
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 # from src import create_app
 
-# # app = create_app()
+# app = create_app()
 
-# # if __name__ == '__main__':
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 import sys
@@ -13,5 +30,6 @@ from src import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Utilise le port de Render (ou 5000 en local)
+    app.run(host="0.0.0.0", port=port, debug=True)  # Le host doit être "0.0.0.0"
